f27fc184136b8add2c8946b96cc4ec5a4a051ba6
yanran.hfs_0 <yanran.hfs@OceanBase036196.sqa.cm4>
2012-09-25 15:21:36 +0800

    don't store pointers in list, store objects directly

--- bootstrap.sh
+++ bootstrap.sh
@@ -1,3 +1,5 @@
 #g++ -o main -O3 -g main.cpp -lpthread && time ./main 16 sowpods.txt out.txt
 g++ -o main_sp -O3 -g main_sp.cpp -lpthread && time ./main_sp 16 sowpods.txt out.txt
 #g++ -o main_sp -O3 -g main_sp.cpp -lpthread && time ./main_sp 16 mini.txt out.txt
+g++ -o main_sp2 -O3 -g main_sp2.cpp -lpthread && time ./main_sp2 16 sowpods.txt out.txt
+#g++ -o main_sp2 -O3 -g main_sp2.cpp -lpthread && time ./main_sp2 16 mini.txt out.txt
